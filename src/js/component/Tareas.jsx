60c3a87e5271bf3cb40f058645aaa94769640d21
Alba <allbaluccia@gmail.com>
2024-08-15 22:01:01 +0000

    corrigiendo

--- src/js/component/Tareas.jsx
+++ src/js/component/Tareas.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 
+
 const Tareas = () => {
     const [todos, setTodos] = useState([]);
     const [tareasValue, setTareasValue] = useState("");
+    const [user, setUser] = useState ("");
+    const [userName, setUserName] = useState ('Alba');
 
 
 
@@ -13,48 +16,56 @@ const Tareas = () => {
         alert("tarea guardada")
 
     }
-    const handleDelete = (index) => {
-        const newArray = [...todos]
-        newArray.splice(index, 1)
-        setTodos(newArray)
-    }
-
-    const [user, setUser] = useState ("");
-    const [userName, setUserName] = useState ('');
-
-    const createUser = async () =>{
+   
+    const createUser = async (user) =>{
         try {
-            const response = await fetch ('https://playground.4geeks.com/todo/users/Alba',{
+            const response = await fetch (`https://playground.4geeks.com/todo/users/${user}`,{
                 method: "POST",
                 headers: {
                     "accept": "application/json"
                 }
+                
             })
+            
             const result = await response.json()
             setUserName(user)
             setUser('')
-            getUser()
-            console.log(result)
+            console.log('usuario creado', result)
+            getList()
         } catch (error) {
             console.log(error, "error user");
         }
     }
-    const getUser = async () =>{
+    const getUser = async (user) =>{
         try {
-            const response = await fetch (`https://playground.4geeks.com/todo/users/${user}`)
+            const response = await fetch (`https://playground.4geeks.com/todo/users/${userName}`)
             const result = await response.json()
+            console.log('usuario obtenido', result);
             
         } catch (error) {
-            console.log(error);
+            console.log('error no obtine usuario', error);
+        }
+    }
+    const getList = async () => {
+        try {
+            const response = await fetch (`https://playground.4geeks.com/todo/users/${user}`)
+            const data =  await response.json()
+            setTodos(data.todos || [])
+            console.log(data, 'get data list')
+            
+            
+        } catch (error) {
+            console.log(error, 'list error')
         }
     }
     const TodoListF = async () => {
+        if (tareasValue.trim()==='') return
+        const bodyToDo = {
+            "label": tareasValue,
+            "is_done": false
+        }
         try {
-            const bodyToDo = {
-                "label": tareasValue,
-                "is_done": false
-            }
-            const response = await fetch("https://playground.4geeks.com/todo/todos/Alba", {
+            const response = await fetch(`https://playground.4geeks.com/todo/todos/${userName}`, {
                 method: "POST",
                 headers: {
                     "accept": "application/json",
@@ -65,59 +76,43 @@ const Tareas = () => {
             })
 
             const result = await response.json()
+            setTareasValue('')
             getList()
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const getList = async () => {
-        try {
-            const getTodo = await fetch ('https://playground.4geeks.com/todo/users/Alba')
-            const data =  await getTodo.json()
-            const dataList = data
-            console.log(dataList)
-            setTodos(dataList.todos)
+            alert('tarea guardada')
+            console.log(result, 'tarea result');
+            
             
         } catch (error) {
-            console.log(error)
+            console.log(error, 'error tarea guardada')
         }
     }
 
-    const putList = async (index) => {
+    
+    const putList = async (index, todo) => {
+        const updatedTodo = {
+            ...todo,
+            "label": tareasValue.trim().length === 0 ? el.label : tareasValue,
+            "is_done": todo.is_done,
+        }
         try {
-
-            const putTodo = {
-                "label": tareasValue.trim().length === 0 ? el.label : tareasValue,
-                "is_done": false,
-                "id": 33 
-            }
-                console.log(id)
-            const obtainPut = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+            
+            const response = await fetch(`https://playground.4geeks.com/todo/todos/${todo.id}`, {
                 method: "PUT",
                 headers: {
                     "Accept": "application/json",
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify(putTodo)
+                body: JSON.stringify(updatedTodo)
             })
-    
+            
             const obtainResult = await obtainPut.json() 
-            console.log(obtainResult)
+            console.log(obtainResult, 'obtain result')
+            getList()
         } catch (error) {
-            console.log(error)
+            console.log(error, 'error putlist')
         }
     }
     
-
-useEffect(() => {
-    getList()
-    getUser()
-},[])
-
-
-
-
     const deleteList = async (id) => {
         try {
             const response = await fetch (`https://playground.4geeks.com/todo/todos/${id}`, {
@@ -129,11 +124,20 @@ useEffect(() => {
             })
             const result = await response.text()
             getList()
-            console.log(result)
+            console.log(result, 'tarea eliminada')
         } catch (error) {
-            console.log(error)
+            console.log(error, 'error al eliminar tarea')
         }
     }
+
+useEffect(() => {
+    getList()
+    
+},[userName])
+
+
+
+
 
 
 
@@ -150,7 +154,7 @@ useEffect(() => {
                 onChange={(event) => setUser(event.target.value)}
                 onKeyDown={(event) => {
                     if(event.key === "Enter"){
-                        createUser()
+                        createUser(user)
                     }
                 }}
                 placeholder="add user">
@@ -179,10 +183,11 @@ useEffect(() => {
                 {todos.length > 0 ? ( 
                     todos?.map((todo, index) => {
                         return (
-                            <li key={index}>{todo.label}
+                            <li key={todo.id}>
+                                {todo.label}
 
                                 <button className="x" onClick={() => deleteList(todo.id)}>x</button>
-                                
+                                <button onClick={() => putList(index, todo)}>Edit</button>
                             </li>
                         )
                     })
